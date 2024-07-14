8d6aad16ddc2f174eca243af126f3587f144a47b
qwertyzer <kirillmailan30@gmail.com>
2024-07-15 01:42:03 +0300

    Water_Bottles done

--- LeetCode/Easy/Water_Bottles/Water_Bottles.cpp
+++ LeetCode/Easy/Water_Bottles/Water_Bottles.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+
+int numWaterBottles(int numBottles, int numExchange);
+
+int main() {
+  int numBottles, numExchange;
+  std::cin >> numBottles >> numExchange;
+  std::cout << numWaterBottles(numBottles, numExchange) << std::endl;
+  return 0;
+}
+
+int numWaterBottles(int numBottles, int numExchange) {
+  int drinkBottles = numBottles, emptyBottles = 0;
+  while (numBottles + emptyBottles >= numExchange) {
+    emptyBottles = numBottles + emptyBottles;
+    numBottles = emptyBottles / numExchange;
+    emptyBottles = emptyBottles - (numBottles * numExchange);
+    drinkBottles += numBottles;
+  }
+  return drinkBottles;
+}
