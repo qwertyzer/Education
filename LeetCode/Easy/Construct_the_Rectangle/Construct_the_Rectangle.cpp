8a3d0a47df21ec4bdb08e80c08ed50d70e4f2026
qwertyzer <kirillmailan30@gmail.com>
2024-07-19 16:35:02 +0300

    .

--- LeetCode/Easy/Construct_the_Rectangle/Construct_the_Rectangle.cpp
+++ LeetCode/Easy/Construct_the_Rectangle/Construct_the_Rectangle.cpp
@@ -1,10 +1,13 @@
+#include <cmath>
 #include <iostream>
 #include <vector>
 
 std::vector<int> constructRectangle(int area);
 
 int main() {
-  std::vector<int> result;
+  int area;
+  std::cin >> area;
+  std::vector<int> result = constructRectangle(area);
   for (int i : result) {
     std::cout << i << " ";
   }
@@ -13,5 +16,18 @@ int main() {
 
 std::vector<int> constructRectangle(int area) {
   std::vector<int> result;
+  int sqrt_area = sqrt(area);
+  if (sqrt(area) == floor(sqrt(area))) {
+    auto iter = result.cbegin();
+    result.insert(iter, 2, sqrt(area));
+  } else {
+    for (int i = sqrt_area; i >= 1; i--) {
+      if (area % i == 0) {
+        result.push_back(area / i);
+        result.push_back(i);
+        break;
+      }
+    }
+  }
   return result;
 }
