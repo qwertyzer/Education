cc2aff01dacf5676a7977f0f4f7b52ca540362c2
qwertyzer <kirillmailan30@gmail.com>
2024-07-19 13:35:52 +0300

    .

--- LeetCode/Easy/Reverse_String_II/Reverse_String_II.cpp
+++ LeetCode/Easy/Reverse_String_II/Reverse_String_II.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 
@@ -7,8 +8,19 @@ int main() {
   std::string s;
   int k;
   std::cin >> s;
+  std::cin >> k;
   std::cout << reverseStr(s, k) << std::endl;
   return 0;
 }
 
-std::string reverseStr(std::string s, int k) {}
+std::string reverseStr(std::string s, int k) {
+  int n = s.length();
+  for (int i = 0; i < n; i += 2 * k) {
+    if (i + k <= n) {
+      std::reverse(s.begin() + i, s.begin() + i + k);
+    } else {
+      std::reverse(s.begin() + i, s.end());
+    }
+  }
+  return s;
+}
