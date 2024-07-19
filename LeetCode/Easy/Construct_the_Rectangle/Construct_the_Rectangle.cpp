00074392195d29760a5a5e4babb4ce9ea2b591eb
qwertyzer <kirillmailan30@gmail.com>
2024-07-19 13:47:13 +0300

    .

---
+++ LeetCode/Easy/Construct_the_Rectangle/Construct_the_Rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> constructRectangle(int area);
+
+int main() {
+  std::vector<int> result;
+  for (int i : result) {
+    std::cout << i << " ";
+  }
+  return 0;
+}
+
+std::vector<int> constructRectangle(int area) {
+  std::vector<int> result;
+  return result;
+}
---
+++ LeetCode/Easy/Construct_the_Rectangle/README.md
@@ -0,0 +1,1 @@
+![Task](../img/Construct_the_Rectangle.png)
